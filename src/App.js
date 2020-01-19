e68c4cb040690aaf5d1e628e0c7382df04f46313
Declan Ramsay <declan@feast-it.com>
2020-01-19 19:23:04 +0000

    POC homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="headroom">
+        <div className="container">
+          <img src="quk-logo.png" alt="Quidditch UK" className="logo" />
+        </div>
+      </div>
+      <header className="v-header container">
+        <div className="fullscreen-video-wrap">
+          <video src="video.mp4" autoPlay loop muted />
+        </div>
+
+        <div className="header-overlay" />
+        <div className="header-content">
+          <h1>Find your passion.</h1>
+          <input type="text" placeholder="Postcode" className="postcode" /> <button type="button">Find Quidditch</button>
+        </div>
       </header>
+
+      <div className="section-one container">
+        <h2>Latest News</h2>
+
+        <div className="grid">
+          <div className="block">
+            <div className="img-container">
+              <img src="1.jpg" alt="image one" />
+            </div>
+
+            <p>This is the latest news for the quidditch uk ...Read More</p>
+          </div>
+
+          <div className="block">
+            <div className="img-container">
+              <img src="2.jpg" alt="image one" />
+            </div>
+
+            <p>This is the latest news for the quidditch uk ...Read More</p>
+          </div>
+
+          <div className="block">
+            <div className="img-container">
+              <img src="3.jpg" alt="image one" />
+            </div>
+
+            <p>This is the latest news for the quidditch uk ...Read More</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
