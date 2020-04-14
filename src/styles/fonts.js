f2ea92a77260af38bbe75b2cef26f4146c9ae687
Declan Ramsay <declandr@gmail.com>
2020-04-14 14:25:01 +0100

    Add cape sans and fix font loading for pt-sans

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -4,6 +4,14 @@ export default `
     font-family: 'pt-sans';
     font-style: normal;
     font-weight: normal;
-    src: url('/fonts/PTSans-Regular.ttf') format('ttf');
+    src: url('fonts/PTSans-Regular.ttf') format('truetype');
+  }
+
+  @font-face {
+    font-display: swap;
+    font-family: 'cape-sans';
+    font-style: normal;
+    font-weight: normal;
+    src: url('fonts/cape.ttf') format('truetype');
   }
 `;
