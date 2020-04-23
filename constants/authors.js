f62c8bc316975c00ba1f7c18986451fa889710f4
Declan Ramsay <declandr@gmail.com>
2020-04-23 22:59:22 +0100

    Upload new authors headshots

--- constants/authors.js
+++ constants/authors.js
@@ -1,8 +1,12 @@
 const SamInstone = '../images/authors/sam-instone.jpeg';
+const MattBateman = '../images/authors/matt-bateman.jpg';
+const AbbyWhiteley = '../images/authors/abby-whiteley.jpg';
 const Logo = '../images/logo.png';
 
 const authors = {
   'Sam Instone': SamInstone,
+  'Abby Whiteley': AbbyWhiteley,
+  'Matt Bateman': MattBateman,
 };
 
 export default function (author) {
