d2ea739b75705c47cd9e563ce0ae96f06c8e63ab
Declan Ramsay <declandr@gmail.com>
2020-06-11 21:12:04 +0100

    Create new event layout with description

--- pages/events/[event].js
+++ pages/events/[event].js
@@ -2,13 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useRouter } from 'next/router';
 import { format } from 'date-fns';
-import { Box } from 'components/layout';
+import parse from 'html-react-parser';
+import styled from 'styled-components';
+import { Box, Flex } from 'components/layout';
 import Page404 from 'pages/404';
 import Layout from 'containers/layout';
 import Meta from 'components/meta';
-import HeroWithLocation from 'components/hero-with-location';
+import Container from 'components/container';
+import { TYPES } from 'components/club-type';
 import PageLoading from 'components/page-loading';
+import Heading from 'components/heading';
+import Content from 'components/content';
 import { parseTimestamptz } from 'modules/dates';
+import { BLOG_MIN_HEIGHTS } from 'styles/hero-heights';
+import { rem } from 'styles/theme';
+import PinIcon from 'public/images/location-pin.svg';
 
 const EVENT = {
   uuid: '36f03565-f622-43e6-90c5-fae022c5444z',
@@ -18,10 +26,42 @@ const EVENT = {
   location: { type: 'POINT', coordinates: ['-2.811808', '56.341305'] },
   venue: 'Sheffield Hallam University Sports Park',
   postcode: 'S9 1UA',
-  start_time: '2020-11-13 07:00:00Z',
+  start_time: '2020-11-14 07:00:00Z',
+  end_time: '2020-11-15 17:00:00Z',
   images: ['https://images.prismic.io/chaser/239db290-616f-4839-8d5f-3fa0ea83ab4d_DSC04508-2000x1200.jpg?auto=compress,format'],
   icon: 'https://images.prismic.io/chaser/65d65868-3e13-4024-871a-6f23d1467042_Northern-Cup-2019-Logo.png?auto=compress,format',
+  description: '<p>Welcome to Northern Cup 2019! The UK’s Northern teams will be competing for the title of Northern Cup Champions, as well as fighting to earn their place at the inaugural European Qualifying Tournament and the British Quidditch Cup 2019.</p><p> Welcome to Northern Cup 2019! The UK’s Northern teams will be competing for the title of Northern Cup Champions, as well as fighting to earn their place at the inaugural European Qualifying Tournament and the British Quidditch Cup 2019.</p>',
 };
+
+const Icon = styled.img`
+  border-radius: 50%;
+  height: 100px;
+  width: 100px;
+  background: ${({ theme }) => theme.colors.white};
+  box-shadow: ${({ theme }) => theme.shadows.box};
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.m}) {
+    height: 200px;
+    width: 200px;
+  }
+`;
+
+const LocationIcon = styled(PinIcon)`
+  height: 15px;
+  width: 15px;
+`;
+
+const LocationLink = styled.a`
+  padding: ${({ theme }) => theme.space[1]};
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.primary};
+  border-bottom: 2px dotted ${({ theme }) => theme.colors.primary};
+`;
+
+const Time = styled.span`
+  color: ${({ theme }) => theme.colors.secondary};
+  font-weight: bold;
+`;
 
 const EventPage = ({ event }) => {
   const router = useRouter();
@@ -42,23 +82,75 @@ const EventPage = ({ event }) => {
         image={event.images[0]}
       />
 
-      <HeroWithLocation
-        image={event.images[0]}
-        name={event.name}
-        venue={event.venue}
-        featuredColor="primary"
-        textColor="white"
-        icon={event.icon}
-        league={event.league}
-        location={event.location}
-        startTime={event.start_time}
-      />
+      <Box
+        as="section"
+        position="relative"
+        backgroundImage={`url(${event.images[0]})`}
+        backgroundColor="primary"
+        backgroundSize="cover"
+        backgroundPosition="center"
+        minHeight={BLOG_MIN_HEIGHTS}
+      >
+
+        <Flex
+          position="absolute"
+          minHeight={BLOG_MIN_HEIGHTS}
+          zIndex={1}
+          bg={TYPES[event.league]}
+          opacity={0.2}
+          width="100%"
+        />
+
+        <Flex
+          position="relative"
+          minHeight={BLOG_MIN_HEIGHTS}
+          alignItems="center"
+          justifyContent="center"
+          zIndex={2}
+        >
+          <Container maxWidth={rem(960)} textAlign="center" px={{ _: 'gutter._', s: 'gutter.s', m: 'gutter.m' }}>
+            <Icon src={event.icon} alt={`${event.name} logo`} />
+          </Container>
+        </Flex>
+      </Box>
 
       <Box
+        as="section"
+        position="relative"
+        backgroundColor="white"
+        color="primary"
+        px={{ _: 'gutter._', s: 'gutter.s', m: 'gutter.m' }}
+        py={{ _: 'gutter._', s: 3, m: 4 }}
+      >
+        <Container px={{ _: 'gutter._', s: 'gutter.s', m: 'gutter.m' }}>
+          <Flex flexDirection="column">
+            <Heading as="h1" fontSize={[4, 5]} isBody marginTop="0" marginBottom="2">{event.name}</Heading>
+
+            {event.start_time && (<Time>{format(parseTimestamptz(event.start_time), 'EEE, d LLL H:mm a')} – {format(parseTimestamptz(event.end_time), 'EEE, d LLL H:mm a')}</Time>)}
+            <Flex alignItems="center">
+              <LocationIcon />{' '}
+              <LocationLink
+                href={`https://www.google.com/maps/search/?api=1&query=${event.location?.coordinates[1]},${event.location?.coordinates[0]}`}
+                rel="noopener noreferrer"
+                target="_blank"
+                linkColor="primary"
+              >
+                {event.venue}
+              </LocationLink>
+            </Flex>
+          </Flex>
+        </Container>
+      </Box>
+      <Box
         bg="greyLight"
-        px={10}
-        py={20}
-      />
+        px={{ _: 'gutter._', s: 'gutter.s', m: 'gutter.m' }}
+        py={{ _: 'gutter._', s: 'gutter.s', m: 'gutter.m' }}
+      >
+        <Container px={{ _: 'gutter._', s: 'gutter.s', m: 'gutter.m' }}>
+          <Content>{parse(event.description)}</Content>
+        </Container>
+      </Box>
+
     </Layout>
   );
 };
@@ -77,7 +169,9 @@ export const getServerSideProps = async ({ params: { event } }) => {
 EventPage.propTypes = {
   event: PropTypes.shape({
     name: PropTypes.string,
+    description: PropTypes.string,
     start_time: PropTypes.string,
+    end_time: PropTypes.string,
     venue: PropTypes.string,
     icon: PropTypes.string,
     league: PropTypes.string,
