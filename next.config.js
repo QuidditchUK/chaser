68d69b9c2bd3fbc48ed755b8729ea78293ac81a2
Declan Ramsay <declandr@gmail.com>
2020-06-15 23:23:17 +0100

    Add sentry with basic configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,57 @@
+const withSourceMaps = require('@zeit/next-source-maps')();
 const path = require('path');
 
+const SentryWebpackPlugin = require('@sentry/webpack-plugin')
+const {
+  NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN,
+  SENTRY_ORG,
+  SENTRY_PROJECT,
+  SENTRY_AUTH_TOKEN,
+  NODE_ENV,
+} = process.env
+
 module.exports = {
-  webpack: (config) => {
+  webpack: (config, options) => {
+    // In `pages/_app.js`, Sentry is imported from @sentry/node. While
+    // @sentry/browser will run in a Node.js environment, @sentry/node will use
+    // Node.js-only APIs to catch even more unhandled exceptions.
+    //
+    // This works well when Next.js is SSRing your page on a server with
+    // Node.js, but it is not what we want when your client-side bundle is being
+    // executed by a browser.
+    //
+    // Luckily, Next.js will call this webpack function twice, once for the
+    // server and once for the client. Read more:
+    // https://nextjs.org/docs#customizing-webpack-config
+    //
+    // So ask Webpack to replace @sentry/node imports with @sentry/browser when
+    // building the browser's bundle
+    if (!options.isServer) {
+      config.resolve.alias['@sentry/node'] = '@sentry/browser'
+    }
+
+    // When all the Sentry configuration env variables are available/configured
+    // The Sentry webpack plugin gets pushed to the webpack plugins to build
+    // and upload the source maps to sentry.
+    // This is an alternative to manually uploading the source maps
+    // Note: This is disabled in development mode.
+    if (
+      SENTRY_DSN &&
+      SENTRY_ORG &&
+      SENTRY_PROJECT &&
+      SENTRY_AUTH_TOKEN &&
+      NODE_ENV === 'production'
+    ) {
+      config.plugins.push(
+        new SentryWebpackPlugin({
+          include: '.next',
+          ignore: ['node_modules'],
+          urlPrefix: '~/_next',
+          release: options.buildId,
+        })
+      )
+    }
+
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,29 @@
+/* eslint-disable react/prop-types */
+import * as Sentry from '@sentry/node';
 import React from 'react';
-import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
 import dynamic from 'next/dynamic';
-import DocumentHead from '../document/head';
-import theme from '../styles/theme';
+import DocumentHead from 'document/head';
+import theme from 'styles/theme';
+
+Sentry.init({
+  enabled: process.env.NODE_ENV === 'production',
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+});
 
 const Scripts = dynamic(() => import('../document/scripts'), { ssr: false });
 
 require('intersection-observer');
 
-function App({ Component, pageProps }) {
+function App({ Component, pageProps, err }) {
   return (
     <ThemeProvider theme={theme}>
       <DocumentHead />
       <Scripts />
 
-      <Component {...pageProps} />
+      <Component {...pageProps} err={err} />
     </ThemeProvider>
   );
 }
-
-App.propTypes = {
-  Component: PropTypes.func.isRequired,
-  pageProps: PropTypes.shape({}).isRequired,
-};
 
 export default App;
