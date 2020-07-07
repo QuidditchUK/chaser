772a7e18248b7ea42e631474fa69f4261bc6452f
Declan Ramsay <declandr@gmail.com>
2020-07-07 20:36:55 +0100

    Add author images for Kirsty, Jasmine and Declan

--- constants/authors.js
+++ constants/authors.js
@@ -1,12 +1,19 @@
 const SamInstone = '../images/authors/sam-instone.jpeg';
 const MattBateman = '../images/authors/matt-bateman.jpg';
 const AbbyWhiteley = '../images/authors/abby-whiteley.jpg';
+const KirstyScheiby = '../images/authors/kirsty-scheiby.jpg';
+const DeclanRamsay = '../images/authors/declan-ramsay.jpg';
+const JasmineLowen = '../images/author/jasmine-lowen';
+
 const Logo = '../images/logo.png';
 
 const authors = {
   'Sam Instone': SamInstone,
   'Abby Whiteley': AbbyWhiteley,
   'Matt Bateman': MattBateman,
+  'Kirsty Scheiby': KirstyScheiby,
+  'Declan Ramsay': DeclanRamsay,
+  'Jasmine Lowen': JasmineLowen,
 };
 
 export default function (author) {
