f978ecf6f80d6d0f55ae7aa66ae7446c78ac4d3f
Declan Ramsay <declan.ramsay@starlingbank.com>
2020-07-09 19:25:21 +0100

    Re-add preview but wrap in try/catch

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,16 +1,26 @@
 import { Client, linkResolver } from 'modules/prismic';
 
+// eslint-disable-next-line consistent-return
 export default async (req, res) => {
-  const { token } = req.query;
-  if (token) {
-    try {
-      const url = await Client(req).previewSession(token, linkResolver, '/');
-      res.writeHead(302, { Location: url });
-      res.end();
-    } catch {
-      res.status(400).send('Something went wrong with the previewSession request');
+  const { token: ref, documentId } = req.query;
+  try {
+    const redirectUrl = await Client(req).getPreviewResolver(ref, documentId).resolve(linkResolver, '/');
+
+    if (!redirectUrl) {
+      return res.status(401).json({ message: 'Invalid token' });
     }
-  } else {
-    res.status(400).send('Missing token from preview request');
+
+    res.setPreviewData({ ref });
+    // Redirect the user to the share endpoint from same origin. This is
+    // necessary due to a Chrome bug:
+    // https://bugs.chromium.org/p/chromium/issues/detail?id=696204
+    res.write(
+      `<!DOCTYPE html><html><head><meta http-equiv="Refresh" content="0; url=${redirectUrl}" />
+      <script>window.location.href = '${redirectUrl}'</script>
+      </head>`,
+    );
+    res.end();
+  } catch {
+    res.status(400).json({ message: 'Something went wrong' });
   }
 };
