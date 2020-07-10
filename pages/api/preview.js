887bf86b76979488e3ee5257883a389a965a87ec
Declan Ramsay <declan.ramsay@starlingbank.com>
2020-07-10 13:21:14 +0100

    Use Client module instead

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,12 +1,7 @@
-import { linkResolver } from 'modules/prismic';
-import Prismic from 'prismic-javascript';
-import config from '../../config';
+import { Client, linkResolver } from 'modules/prismic';
 
-const { prismic } = config;
-
-// eslint-disable-next-line consistent-return
 export default async (req, res) => {
   const { token, documentId } = req.query;
-  const redirectUrl = await Prismic.api(prismic.url).getPreviewResolver(token, documentId).resolve(linkResolver, '/');
+  const redirectUrl = await Client(req).getPreviewResolver(token, documentId).resolve(linkResolver, '/');
   res.redirect(302, redirectUrl);
 };
