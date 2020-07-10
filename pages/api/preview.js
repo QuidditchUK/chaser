0080d73786493cdaa926690529125cc05cfc6d6c
Declan Ramsay <declan.ramsay@starlingbank.com>
2020-07-10 13:23:47 +0100

    Revert to old preview script

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,7 +1,26 @@
 import { Client, linkResolver } from 'modules/prismic';
 
+// eslint-disable-next-line consistent-return
 export default async (req, res) => {
-  const { token, documentId } = req.query;
-  const redirectUrl = await Client(req).getPreviewResolver(token, documentId).resolve(linkResolver, '/');
-  res.redirect(302, redirectUrl);
+  const { token: ref, documentId } = req.query;
+  try {
+    const redirectUrl = await Client(req).getPreviewResolver(ref, documentId).resolve(linkResolver, '/');
+
+    if (!redirectUrl) {
+      return res.status(401).json({ message: 'Invalid token' });
+    }
+
+    res.setPreviewData({ ref });
+    // Redirect the user to the share endpoint from same origin. This is
+    // necessary due to a Chrome bug:
+    // https://bugs.chromium.org/p/chromium/issues/detail?id=696204
+    res.write(
+      `<!DOCTYPE html><html><head><meta http-equiv="Refresh" content="0; url=${redirectUrl}" />
+      <script>window.location.href = '${redirectUrl}'</script>
+      </head>`,
+    );
+    res.end();
+  } catch {
+    res.status(400).json({ message: 'Something went wrong' });
+  }
 };
