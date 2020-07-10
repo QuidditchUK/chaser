df1da6034d461385e58827f607219a511e62b367
Declan Ramsay <declan.ramsay@starlingbank.com>
2020-07-10 13:28:30 +0100

    Remove redirect hack

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -11,14 +11,6 @@ export default async (req, res) => {
     }
 
     res.setPreviewData({ ref });
-    // Redirect the user to the share endpoint from same origin. This is
-    // necessary due to a Chrome bug:
-    // https://bugs.chromium.org/p/chromium/issues/detail?id=696204
-    res.write(
-      `<!DOCTYPE html><html><head><meta http-equiv="Refresh" content="0; url=${redirectUrl}" />
-      <script>window.location.href = '${redirectUrl}'</script>
-      </head>`,
-    );
     res.end();
   } catch {
     res.status(400).json({ message: 'Something went wrong' });
