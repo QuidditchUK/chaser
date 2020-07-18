c4c2a3acad31d848fd82bb132757293fd89bab98
Declan Ramsay <declandr@gmail.com>
2020-07-18 15:22:28 +0100

    [WIP] dashboard checklist

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,22 +1,134 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Link from 'next/link';
+import { color, border } from 'styled-system';
 import { api } from 'modules/api';
-import Meta from 'components/meta';
-import { Box } from 'components/layout';
-import Heading from 'components/heading';
 import { parseCookies } from 'modules/cookies';
+import Meta from 'components/meta';
+import { Box, Grid, Flex } from 'components/layout';
+import Container from 'components/container';
+import Heading from 'components/heading';
+import Content from 'components/content';
+import Image from 'components/image';
 
-const Dashboard = () => (
-  <>
-    <Meta description="Sign in to QuidditchUK to manage your QuidditchUK Membership, Account details and more" subTitle="Dashboard" />
-    <Box
-      bg="greyLight"
-      py={{ _: 4, l: 10 }}
-      px={{ _: 'gutter._', s: 'gutter.s', m: 'gutter.m' }}
-    >
-      <Heading as="h1" isBody>DASHBOARD</Heading>
-    </Box>
-  </>
-);
+const ListItem = styled.li`
+  ${color};
+  height: 30px;
+`;
+
+const Checkmark = styled.span`
+  margin-left: ${({ theme }) => theme.space[3]};
+  height: 25px;
+  width: 25px;
+  border-radius: 50%;
+  display: inline-block;
+    background-color: ${({ theme }) => theme.colors.keeperGreen};
+
+  &:after {
+    content: "";
+    display: block;
+    position: relative;
+    left: 10px;
+    top: 7px;
+    width: 5px;
+    height: 10px;
+    border: solid ${({ theme }) => theme.colors.white};
+    border-width: 0 3px 3px 0;
+    transform: rotate(45deg);
+  }
+`;
+
+const StyledAnchor = styled.a`
+  text-decoration: none;
+  border-bottom: 2px solid ${({ theme }) => theme.colors.black};
+  ${color};
+  ${border};
+`;
+
+const StyledList = styled.ol``;
+
+const Dashboard = ({ user, products, club }) => {
+  console.log(club);
+  const hasMembership = useRef(!!products.length);
+  const hasClub = useRef(user.club_uuid);
+  const setupProfile = useRef(user.first_name && user.last_name);
+
+  return (
+    <>
+      <Meta description="Sign in to QuidditchUK to manage your QuidditchUK Membership, Account details and more" subTitle="Dashboard" />
+      <Box
+        bg="greyLight"
+        py={{ _: 4, l: 10 }}
+        px={{ _: 'gutter._', s: 'gutter.s', m: 'gutter.m' }}
+      >
+        <Container>
+          <Box bg="white" borderRadius={1}>
+            <Grid
+              gridTemplateColumns={{ _: '1fr', m: '2fr 1fr' }}
+              gridGap={{ _: 'gutter._', m: 'gutter.m' }}
+              bg="white"
+              color="primary"
+              borderRadius={1}
+              overflow="hidden"
+            >
+              <Flex flexDirection="column" padding={4}>
+                <Heading as="h2" isBody color="primary">Get ready for brooms up</Heading>
+                <Content>As the new season approaches, complete the following list to be ready when the season kicks off:</Content>
+
+                <StyledList>
+                  <ListItem color={hasMembership.current ? 'keeperGreen' : 'black'}>
+                    <Flex alignItems="center">
+                      <Link href="/dashboard/membership/manage" passHref>
+                        <StyledAnchor color={hasMembership.current ? 'keeperGreen' : 'black'} borderColor={hasMembership.current ? 'keeperGreen' : 'black'}>
+                          Purchase your QuidditchUK Membership
+                        </StyledAnchor>
+                      </Link>
+
+                      {hasMembership.current && <Checkmark />}
+                    </Flex>
+                  </ListItem>
+
+                  <ListItem color={hasClub.current ? 'keeperGreen' : 'black'}>
+                    <Flex alignItems="center">
+                      <Link href="/dashboard/membership/club" passHref>
+                        <StyledAnchor color={hasClub.current ? 'keeperGreen' : 'black'} borderColor={hasClub.current ? 'keeperGreen' : 'black'}>
+                          Select your club
+                        </StyledAnchor>
+                      </Link>
+
+                      {!!hasClub.current && <Checkmark />}
+                    </Flex>
+                  </ListItem>
+
+                  <ListItem color={setupProfile.current ? 'keeperGreen' : 'black'}>
+                    <Flex alignItems="center">
+                      <Link href="/dashboard/account/profile" passHref>
+                        <StyledAnchor color={setupProfile.current ? 'keeperGreen' : 'black'} borderColor={setupProfile.current ? 'keeperGreen' : 'black'}>
+                          Setup your profile
+                        </StyledAnchor>
+                      </Link>
+
+                      {setupProfile.current && <Checkmark />}
+                    </Flex>
+                  </ListItem>
+                </StyledList>
+              </Flex>
+
+              <Image
+                alt="London Unbreakables line up at the keeper line ready for brooms up"
+                src="https://images.prismic.io/chaser/b97e3eab-dcb7-4474-85e0-914afe58ae74_IMG_0529.JPG?auto=compress,format"
+                height={600}
+                width={900}
+              />
+            </Grid>
+          </Box>
+
+        </Container>
+      </Box>
+    </>
+  );
+};
 
 export const getServerSideProps = async ({ req, res }) => {
   const { AUTHENTICATION_TOKEN } = parseCookies(req);
@@ -28,7 +140,7 @@ export const getServerSideProps = async ({ req, res }) => {
     return { props: {} };
   }
 
-  const { data: product } = await api.get('/products/me', {
+  const { data: products } = await api.get('/products/me', {
     headers: {
       Authorization: `Bearer ${AUTHENTICATION_TOKEN}`,
     },
@@ -47,11 +159,26 @@ export const getServerSideProps = async ({ req, res }) => {
 
   return {
     props: {
-      product,
+      products,
       user,
       club,
     },
   };
+};
+
+Dashboard.defaultProps = {
+  products: [],
+  club: null,
+};
+
+Dashboard.propTypes = {
+  user: PropTypes.shape({
+    club_uuid: PropTypes.string,
+    first_name: PropTypes.string,
+    last_name: PropTypes.string,
+  }).isRequired,
+  products: PropTypes.arrayOf(PropTypes.shape({})),
+  club: PropTypes.shape({}),
 };
 
 export default Dashboard;
