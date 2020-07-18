133f16e06ab869ea132ad68dbe9e053d476a88a1
Declan Ramsay <declandr@gmail.com>
2020-07-18 21:47:23 +0100

    Hook up contact form to backend

--- components/contact-form.js
+++ components/contact-form.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Formik,
   Form,
@@ -7,7 +7,7 @@ import {
 } from 'formik';
 import * as Yup from 'yup';
 import get from 'just-safe-get';
-import { Grid } from 'components/layout';
+import { Grid, Box } from 'components/layout';
 import Input from 'components/input';
 import Label from 'components/label';
 import Textarea from 'components/textarea';
@@ -15,8 +15,10 @@ import Button from 'components/button';
 import PrismicWrapper, { buttonVariants } from 'components/prismic-wrapper';
 import Container from 'components/container';
 import Heading from 'components/heading';
+import Content from 'components/content';
 import Required from 'components/required';
 import { InlineError } from 'components/errors';
+import { api } from 'modules/api';
 import { rem } from 'styles/theme';
 
 const ContactFormSchema = Yup.object().shape({
@@ -25,7 +27,25 @@ const ContactFormSchema = Yup.object().shape({
   message: Yup.string().required('Required'),
 });
 
+const handleSubmit = async (values, setSubmitting, setServerError, setServerSuccess) => {
+  try {
+    setServerError(null);
+    setServerSuccess(null);
+
+    await api.put('/users/me', values);
+
+    setSubmitting(false);
+    setServerSuccess(true);
+  } catch (err) {
+    setServerError(err?.response?.data?.error?.message);
+    setSubmitting(false);
+  }
+};
+
 const ContactForm = (rawData) => {
+  const [serverError, setServerError] = useState(null);
+  const [serverSuccess, setServerSuccess] = useState(null);
+
   const data = {
     variant: get(rawData, 'primary.variant'),
   };
@@ -41,13 +61,7 @@ const ContactForm = (rawData) => {
             subject: '',
             message: '',
           }}
-          onSubmit={(values, { setSubmitting }) => {
-            alert(JSON.stringify(values, null, 2));
-
-            setTimeout(() => {
-              setSubmitting(false);
-            }, 1000);
-          }}
+          onSubmit={(values, { setSubmitting }) => handleSubmit(values, setSubmitting, setServerError, setServerSuccess)}
           validationSchema={ContactFormSchema}
         >
           {({ errors, touched, isSubmitting }) => (
@@ -115,6 +129,18 @@ const ContactForm = (rawData) => {
             </Form>
           )}
         </Formik>
+
+        {serverError && (
+          <>
+            <InlineError my={3}>{serverError}</InlineError>
+          </>
+        )}
+
+        {serverSuccess && (
+          <Box bg="primary" px="4" py="2" mt="6" borderColor="primary" borderWidth="1px" borderStyle="solid" color="white" borderRadius={0}>
+            <Content>Message sent</Content>
+          </Box>
+        )}
       </Container>
     </PrismicWrapper>
   );
