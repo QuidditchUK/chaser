0ca242ad4979eed58d4d69253cb70b9ca1cb6bb6
Declan Ramsay <declandr@gmail.com>
2020-08-04 22:32:00 +0100

    Define gtag in analytics file

--- modules/analytics.js
+++ modules/analytics.js
@@ -1,11 +1,15 @@
 import getConfig from 'next/config';
 
+function gtag(...args) {
+  // eslint-disable-next-line no-unused-expressions
+  window.dataLayer && window.dataLayer.push(args);
+}
 export const pageview = (url) => {
   const { publicRuntimeConfig } = getConfig();
 
   if (process.env.NODE_ENV !== 'development') {
     if (typeof window !== 'undefined') {
-      window.gtag('config', publicRuntimeConfig.gaToken, {
+      gtag('config', publicRuntimeConfig.gaToken, {
         page_path: url,
       });
     }
@@ -21,7 +25,7 @@ export const event = ({
   if (process.env.NODE_ENV !== 'development') {
     try {
       if (typeof window !== 'undefined') {
-        window.gtag('event', action, {
+        gtag('event', action, {
           event_category: category,
           ...(label && { event_label: label }),
           ...(value && { value }),
