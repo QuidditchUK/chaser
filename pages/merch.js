862b056dd4ec75e2dcb1c1e0212ebaf55ced1f69
Declan Ramsay <declandr@gmail.com>
2020-11-26 22:22:25 +0000

    Add metadata to merch page

--- pages/merch.js
+++ pages/merch.js
@@ -1,3 +1,22 @@
+import React from 'react';
 import redirect from 'nextjs-redirect';
+import dynamic from 'next/dynamic';
 
-export default redirect('https://www.utilityapparel.com/quidditch-uk/');
+const Meta = dynamic(() => import('components/meta'));
+const Container = dynamic(() => import('components/container'));
+const Heading = dynamic(() => import('components/heading'));
+const Redirect = redirect('https://www.utilityapparel.com/quidditch-uk/');
+
+export default function Merch() {
+  return (
+    <Redirect>
+      <Meta
+        title="Merch"
+        description="Purchase your official QuidditchUK merch from Utility Apparel"
+      />
+      <Container>
+        <Heading textAlign="center">Redirecting to Utility Apparel...</Heading>
+      </Container>
+    </Redirect>
+  );
+}
