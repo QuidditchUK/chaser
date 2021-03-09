ba302b6d2fd56c4018d3c8746f32315925bbd8c9
Declan Ramsay <declan.ramsay@starlingbank.com>
2021-03-09 12:00:39 +0000

    Remove simple-react-lightbox in favour of chakra modal

--- components/image-slice.js
+++ components/image-slice.js
@@ -1,26 +1,48 @@
 import get from 'just-safe-get';
-import SimpleReactLightbox, { SRLWrapper } from 'simple-react-lightbox';
 import PrismicWrapper from 'components/prismic-wrapper';
-import { Box, Grid, Text } from 'components';
+import {
+  Box,
+  Grid,
+  Text,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+} from 'components';
 import Image from 'components/carousel-image';
 
 const Item = ({ item }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { height, width } = item.image?.dimensions;
 
   return (
-    <Box>
-      <Image
-        alt={item.image?.alt}
-        src={item.image?.url}
-        height={height}
-        width={width}
-      />
-      {item.support && (
-        <Text textAlign="center" pt={2} fontStyle="italic">
-          {item.support}
-        </Text>
-      )}
-    </Box>
+    <>
+      <Box onClick={onOpen} cursor="pointer">
+        <Image
+          alt={item.image?.alt}
+          src={item.image?.url}
+          height={height}
+          width={width}
+        />
+        {item.support && (
+          <Text textAlign="center" pt={2} fontStyle="italic">
+            {item.support}
+          </Text>
+        )}
+      </Box>
+
+      <Modal isOpen={isOpen} size="full" onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent mx={{ base: 4, xl: 20 }} h="initial" bg="none">
+          <Image
+            alt={item.image?.alt}
+            src={item.image?.url}
+            height={height}
+            width={width}
+          />
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
@@ -32,26 +54,22 @@ const ImageSlice = (rawData) => {
 
   return (
     <PrismicWrapper variant={variant} small>
-      <SimpleReactLightbox>
-        <SRLWrapper options={{ settings: { lightboxTransitionSpeed: 0.3 } }}>
-          <Grid
-            gridTemplateColumns={{
-              base: '1fr',
-              md: `${multipleImages ? '1fr 1fr' : '1fr'}`,
-            }}
-            gridGap={{ base: 4, md: 9 }}
-          >
-            {items.map((itemData, i) => {
-              const item = {
-                image: get(itemData, 'image'),
-                support: get(itemData, 'support'),
-              };
+      <Grid
+        gridTemplateColumns={{
+          base: '1fr',
+          md: `${multipleImages ? '1fr 1fr' : '1fr'}`,
+        }}
+        gridGap={{ base: 4, md: 9 }}
+      >
+        {items.map((itemData, i) => {
+          const item = {
+            image: get(itemData, 'image'),
+            support: get(itemData, 'support'),
+          };
 
-              return <Item key={`image-slice-${i}`} item={item} />;
-            })}
-          </Grid>
-        </SRLWrapper>
-      </SimpleReactLightbox>
+          return <Item key={`image-slice-${i}`} item={item} />;
+        })}
+      </Grid>
     </PrismicWrapper>
   );
 };
