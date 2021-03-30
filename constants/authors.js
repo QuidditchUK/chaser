57acf57f1be72c824f72a6b0f3289a0d2c17af0b
Declan Ramsay <declan.ramsay@starlingbank.com>
2021-03-30 11:14:59 +0100

    Dynamic imports of components in blog support and hero

--- constants/authors.js
+++ constants/authors.js
@@ -1,11 +1,11 @@
-const SamInstone = '../images/authors/sam-instone.jpeg';
-const MattBateman = '../images/authors/matt-bateman.jpg';
-const AbbyWhiteley = '../images/authors/abby-whiteley.jpg';
-const KirstyScheiby = '../images/authors/kirsty-scheiby.jpg';
-const DeclanRamsay = '../images/authors/declan-ramsay.jpg';
-const JasmineLowen = '../images/authors/jasmine-lowen.jpg';
-const GeorgiaChallinor = '../images/authors/georgia-challinor.jpg';
-const Logo = '../images/authors/quk-logo.png';
+const SamInstone = '/images/authors/sam-instone.jpeg';
+const MattBateman = '/images/authors/matt-bateman.jpg';
+const AbbyWhiteley = '/images/authors/abby-whiteley.jpg';
+const KirstyScheiby = '/images/authors/kirsty-scheiby.jpg';
+const DeclanRamsay = '/images/authors/declan-ramsay.jpg';
+const JasmineLowen = '/images/authors/jasmine-lowen.jpg';
+const GeorgiaChallinor = '/images/authors/georgia-challinor.jpg';
+const Logo = '/images/authors/quk-logo.png';
 
 const authors = {
   'Sam Instone': SamInstone,
