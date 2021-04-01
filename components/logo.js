0edd544a01778e3d17bf9bf9a8cde755081d1bbc
Declan Ramsay <declandr@gmail.com>
2021-04-01 22:08:29 +0100

    Upgrade to Next@10.1 and switch logo component to hold all the image information and use the loader

--- components/logo.js
+++ components/logo.js
@@ -1,8 +1,33 @@
-import { Link as ChakraLink, Image } from '@chakra-ui/react';
+import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import dynamic from 'next/dynamic';
+const Image = dynamic(() => import('components/image'));
+const logo = '/images/logo.png';
+const logoText = '/images/logo-text.png';
 
-export const LogoLink = (props) => (
-  <ChakraLink height={{ base: '35px', xl: '45px' }} {...props} />
-);
-export const Logo = (props) => (
-  <Image height={{ base: '35px', xl: '45px' }} {...props} />
-);
+export default function Logo({ filter = false }) {
+  const logoHeight = useBreakpointValue({ base: 35, xl: 45 }) || 35;
+  const logoTextWidth = useBreakpointValue({ base: 175, xl: 225 }) || 175;
+
+  return (
+    <Flex alignItems="center">
+      <Image
+        priority={true}
+        layout="fixed"
+        height={logoHeight}
+        width={logoHeight}
+        src={logo}
+        alt="Quidditch UK"
+        filter={filter ? 'brightness(0) invert(1)' : 'inherit'}
+      />
+      <Image
+        priority={true}
+        layout="fixed"
+        height={logoHeight}
+        width={logoTextWidth}
+        src={logoText}
+        alt="Quidditch UK"
+        filter={filter ? 'brightness(0) invert(1)' : 'inherit'}
+      />
+    </Flex>
+  );
+}
--- components/navigation.js
+++ components/navigation.js
@@ -15,10 +15,7 @@ import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 
 import { MAIN_NAVIGATION, DASHBOARD_NAVIGATION } from 'constants/navigation';
 
-const logo = '/images/logo.png';
-const logoText = '/images/logo-text.png';
-
-const Logo = dynamic(() => import('components/logo').then(({ Logo }) => Logo));
+const Logo = dynamic(() => import('components/logo'));
 const DesktopNav = dynamic(() => import('components/navigation/desktop'));
 const DesktopCTAs = dynamic(() =>
   import('components/navigation/desktop').then(({ DesktopCTAs }) => DesktopCTAs)
@@ -79,21 +76,13 @@ export default function Navigation({ dashboard = false }) {
           width="100%"
         >
           <NextLink href="/" passHref>
-            <Link height={{ base: '35px', xl: '45px' }} onClick={onClose}>
-              <Logo
-                height={{ base: '35px', xl: '45px' }}
-                width={{ base: '35px', xl: '45px' }}
-                src={logo}
-                alt="Quidditch UK"
-                filter={dashboard ? 'brightness(0) invert(1)' : 'inherit'}
-              />
-              <Logo
-                height={{ base: '35px', xl: '45px' }}
-                width={{ base: '175px', xl: '225px' }}
-                src={logoText}
-                alt="Quidditch UK"
-                filter={dashboard ? 'brightness(0) invert(1)' : 'inherit'}
-              />
+            <Link
+              height={{ base: '35px', xl: '45px' }}
+              onClick={onClose}
+              display="flex"
+              alignItems="center"
+            >
+              <Logo filter={dashboard} />
             </Link>
           </NextLink>
 
