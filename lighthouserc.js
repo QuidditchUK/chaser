1300b8b72b789b89aa963ef51c11833974083c4c
Declan Ramsay <declandr@gmail.com>
2021-04-02 15:07:04 +0100

    Add start command

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,9 @@
 module.exports = {
   ci: {
+    collect: {
+      startServerCommand: 'npm run start',
+      startServerReadyPattern: 'ready on',
+    },
     upload: {
       target: 'temporary-public-storage',
     },
