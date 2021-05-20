8186e88c27b9102d2c09ccb5d642b5c1342ed24b
Declan Ramsay <declandr@gmail.com>
2021-05-20 20:37:02 +0100

    Remove unused heights and private prop on package

--- styles/hero-heights.js
+++ styles/hero-heights.js
@@ -1,4 +1,2 @@
-export const FIND_QUIDDITCH_MIN_HEIGHTS = { base: '200px', md: '300px' };
 export const BLOG_MIN_HEIGHTS = { base: '250px', md: '400px' };
-export const SLICE_MIN_HEIGHTS = { base: '300px', md: '400px' };
 export const HERO_MIN_HEIGHTS = { base: '250px', md: '540px' };
