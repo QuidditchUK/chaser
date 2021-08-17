0e06b294f8dbc858bd3f504dd7078fe82b6ae376
Declan Ramsay <declandr@gmail.com>
2021-08-17 22:10:10 +0100

    Fix google sheets lookup, temp disable search

--- modules/sheets.js
+++ modules/sheets.js
@@ -1,19 +1,30 @@
-import GSheetsReader from 'g-sheets-api';
+import axios from 'axios';
 
-export default async function getSheet(sheetId, sheetNumber = 1, options = {}) {
-  let result;
-
-  await GSheetsReader(
-    {
-      sheetId,
-      sheetNumber,
-
-      ...options,
-    },
-    (results) => {
-      result = results;
-    }
+// export default async function getSheet(sheetId, sheetName, options = {}) {
+export default async function getSheet(sheetId, sheetName) {
+  const { data } = await axios(
+    `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${sheetName}?key=AIzaSyBBXBSVcXfPZjPXF5JCpY6nghR3ORoigDo`
   );
+  // seperate key row from content rows
+  const [keys, ...rows] = data.values;
 
-  return result;
+  const videos = rows.map((row) => {
+    if (!Array.isArray(row)) {
+      return row;
+    }
+
+    // construct keypairs e.g. [{ 'Team 1': 'Steelfins' },...]
+    const keyPairs = row.map((value, i) => {
+      const key = keys[i];
+      return { [key]: value };
+    });
+
+    return keyPairs.reduce((result, item) => {
+      const key = Object.keys(item)[0]; //first property: 'Team 1', 'Team 2', 'Date'
+      result[key] = item[key];
+      return result;
+    });
+  });
+
+  return videos;
 }
