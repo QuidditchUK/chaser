a8a64c7a1e31d29c04ed740858425ecbc565138f
Declan Ramsay <declandr@gmail.com>
2021-08-18 23:02:38 +0100

    Update sheets to v2

--- modules/sheets.js
+++ modules/sheets.js
@@ -1,31 +1,19 @@
-import axios from 'axios';
+import GSheetsReader from 'g-sheets-api';
 
-// export default async function getSheet(sheetId, sheetName, options = {}) {
-export default async function getSheet(sheetId, sheetName) {
-  const { data } = await axios(
-    `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${sheetName}?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}`
+export default async function getSheet(sheetId, sheetNumber = 1, options = {}) {
+  let result;
+
+  await GSheetsReader(
+    {
+      sheetId,
+      sheetNumber,
+      apiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+      ...options,
+    },
+    (results) => {
+      result = results;
+    }
   );
 
-  // seperate key row from content rows
-  const [keys, ...rows] = data.values;
-
-  const videos = rows.map((row) => {
-    if (!Array.isArray(row)) {
-      return row;
-    }
-
-    // construct keypairs e.g. [{ 'Team 1': 'Steelfins' },...]
-    const keyPairs = row.map((value, i) => {
-      const key = keys[i];
-      return { [key]: value };
-    });
-
-    return keyPairs.reduce((result, item) => {
-      const key = Object.keys(item)[0]; //first property: 'Team 1', 'Team 2', 'Date'
-      result[key] = item[key];
-      return result;
-    });
-  });
-
-  return videos;
+  return result;
 }
