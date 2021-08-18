b04d8135ce6db4eff64c250acbf0e188d82b5ca3
Declan Ramsay <declandr@gmail.com>
2021-08-18 23:28:59 +0100

    Switch to build env variables (#149)
    * Switch to build env variables
    
    * Tweak lighthouse CI run step for yarn
    
    * Switch to use Node 14 for lighthouse
    
    * Switch to using npm see if that helps
    
    * Add start command
    
    * Add secret and url
    
    * Try github-action-comment approach
    
    * Just homepage for now

--- modules/stripe.js
+++ modules/stripe.js
@@ -1,6 +1,3 @@
 import { loadStripe } from '@stripe/stripe-js';
-import getConfig from 'next/config';
 
-const { publicRuntimeConfig } = getConfig();
-
-export const stripePromise = loadStripe(publicRuntimeConfig.stripeToken);
+export const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_TOKEN);
