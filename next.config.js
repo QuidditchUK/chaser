6b7da6e4366bdb7c1b9332934a8af4781b8b5b78
Declan Ramsay <declan.ramsay@starlingbank.com>
2022-03-28 18:06:00 +0100

    Update nextjs, react, sentry dependencies

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const withOffline = require('next-offline');
-
 // This file sets a custom webpack configuration to use your Next.js app
 // with Sentry.
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
@@ -19,84 +17,60 @@ const SentryWebpackPluginOptions = {
   // https://github.com/getsentry/sentry-webpack-plugin#options.
 };
 
-module.exports = withSentryConfig(
-  withOffline({
-    target: 'serverless',
-    transformManifest: (manifest) => ['/'].concat(manifest), // add the homepage to the cache
-    generateInDevMode: false,
-    workboxOpts: {
-      swDest: 'static/service-worker.js',
-      runtimeCaching: [
-        {
-          urlPattern: /^https?.*/,
-          handler: 'NetworkFirst',
-          options: {
-            cacheName: 'https-calls',
-            networkTimeoutSeconds: 15,
-            expiration: {
-              maxEntries: 150,
-              maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
-            },
-            cacheableResponse: {
-              statuses: [0, 200],
-            },
-          },
-        },
-      ],
-    },
-    async rewrites() {
-      return [
-        {
-          source: '/merch',
-          destination: 'https://www.utilityapparel.com/quidditch-uk/',
-        },
-        {
-          source: '/play/coaching-resources',
-          destination:
-            'https://docs.google.com/spreadsheets/d/1n_j-gDhC4mE2j4nTkBAhY-8MlvqAX0bLnWm0on54GJA/',
-        },
-      ];
-    },
-    async redirects() {
-      return [
-        {
-          source: '/referees',
-          destination: '/volunteer/referees',
-          permanent: true,
-        },
-        {
-          source: '/snitches',
-          destination: '/volunteer/snitches',
-          permanent: true,
-        },
-        {
-          source: '/clubs',
-          destination: '/find-quidditch',
-          permanent: true,
-        },
-        {
-          source: '/events',
-          destination: '/find-quidditch',
-          permanent: true,
-        },
-        {
-          source: '/teamuk',
-          destination: '/programmes/team-england',
-          permanent: true,
-        },
-      ];
-    },
-    images: {
-      domains: ['images.prismic.io', 'chaser.cdn.prismic.io'],
-    },
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      });
+const moduleExports = {
+  async rewrites() {
+    return [
+      {
+        source: '/merch',
+        destination: 'https://www.utilityapparel.com/quidditch-uk/',
+      },
+      {
+        source: '/play/coaching-resources',
+        destination:
+          'https://docs.google.com/spreadsheets/d/1n_j-gDhC4mE2j4nTkBAhY-8MlvqAX0bLnWm0on54GJA/',
+      },
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: '/referees',
+        destination: '/volunteer/referees',
+        permanent: true,
+      },
+      {
+        source: '/snitches',
+        destination: '/volunteer/snitches',
+        permanent: true,
+      },
+      {
+        source: '/clubs',
+        destination: '/find-quidditch',
+        permanent: true,
+      },
+      {
+        source: '/events',
+        destination: '/find-quidditch',
+        permanent: true,
+      },
+      {
+        source: '/teamuk',
+        destination: '/programmes/team-england',
+        permanent: true,
+      },
+    ];
+  },
+  images: {
+    domains: ['images.prismic.io', 'chaser.cdn.prismic.io'],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
 
-      return config;
-    },
-  }),
-  SentryWebpackPluginOptions
-);
+    return config;
+  },
+};
+
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
