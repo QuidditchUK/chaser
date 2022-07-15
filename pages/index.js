b9d0a792ff2389fda52e1110736e66a352fd36e7
Declan Ramsay <declan.ramsay@starlingbank.com>
2022-07-15 09:09:57 +0100

    Remove log

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
+import SchedulerFeed from 'components/prismic/scheduler-feed';
 import { getStaticPrismicProps } from 'modules/prismic';
 import dynamic from 'next/dynamic';
 
 const PrismicPage = dynamic(() => import('components/shared/prismic-page'));
 
-const Home = (props) => {
-  return <PrismicPage type="pages" uid="home" {...props} />;
-};
+const Home = (props) => (
+  <>
+    <PrismicPage type="pages" uid="home" {...props} />
+    <SchedulerFeed
+      primary={{
+        scheduler_url: {
+          url:
+            'https://develop--quidditchscheduler.netlify.app/api/game/tournament/32',
+        },
+      }}
+    />
+  </>
+);
 
 export const getStaticProps = async ({
   preview = null,
