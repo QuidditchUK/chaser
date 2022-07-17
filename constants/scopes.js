8288db4a410fa1373a86d8e980c56f5ac9d237ed
Declan Ramsay <declan.ramsay@starlingbank.com>
2022-07-17 16:28:21 +0100

    Add national team and scouting scopes

--- constants/scopes.js
+++ constants/scopes.js
@@ -1,11 +1,21 @@
 export const ADMIN = 'admin';
 export const VOLUNTEER = 'volunteer';
 export const EMT = 'emt';
+export const HEAD_SCOUT = 'head-scout';
 
 export const USERS_READ = 'users:read';
 export const CLUBS_READ = 'clubs:read';
 export const CLUBS_WRITE = 'clubs:write';
 export const TRANSFER_READ = 'transfer:read';
 export const TRANSFER_WRITE = 'transfer:write';
+
+export const NATIONAL_TEAM_READ = 'national-team:read';
+
+export const SCOUTING_ENGLAND_READ = 'scouting-england:read';
+export const SCOUTING_ENGLAND_WRITE = 'scouting-england:write';
+export const SCOUTING_WALES_READ = 'scouting-wales:read';
+export const SCOUTING_WALES_WRITE = 'scouting-wales:write';
+export const SCOUTING_SCOTLAND_READ = 'scouting-scotland:read';
+export const SCOUTING_SCOTLAND_WRITE = 'scouting-scotland:write';
 
 export const DASHBOARD_SCOPES = [VOLUNTEER, EMT, ADMIN];
