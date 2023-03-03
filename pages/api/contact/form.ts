dcb149dbc6a597110fc4d0a6a82c90389986e401
Declan Ramsay <declan.ramsay@starlingbank.com>
2023-03-03 09:32:12 +0000

    Add log to sendEmail for contact form

--- pages/api/contact/form.ts
+++ pages/api/contact/form.ts
@@ -9,15 +9,19 @@ interface Request extends NextApiRequest {
 export default async function handler(req: Request, res: NextApiResponse) {
   switch (req.method) {
     case 'POST':
-      sendEmail({
-        template: 'contactForm',
-        to: 'secretary@quidditchuk.org',
-        cc: 'admin@quidditchuk.org',
-        data: req.body,
-      });
+      try {
+        await sendEmail({
+          template: 'contactForm',
+          to: 'secretary@quidditchuk.org',
+          cc: 'admin@quidditchuk.org',
+          data: req.body,
+        });
 
-      res.status(200).end();
-      return;
+        res.status(200).end();
+        return;
+      } catch (error) {
+        console.log(error);
+      }
     default:
       res.status(404).end();
       return;
